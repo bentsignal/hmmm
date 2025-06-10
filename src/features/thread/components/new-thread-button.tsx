82539ae08004f1509d5128906ae66dd8355e89be
Shawn Rodgers <me@bentsignal.com>
2025-06-10 11:33:57 -0400

    ...

--- src/features/thread/components/new-thread-button.tsx
+++ src/features/thread/components/new-thread-button.tsx
@@ -2,12 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-// import { useQuery } from "convex/react";
-// import { api } from "@/convex/_generated/api";
 
 export default function NewThreadButton() {
   const router = useRouter();
-  // const threads = useQuery(api.thread.getThreads);
   return (
     <Button
       className="w-full"
