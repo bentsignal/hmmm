8812e3155d8a924f72dff93d4d6e1cbf76701659
Shawn Rodgers <me@bentsignal.com>
2025-06-11 18:51:10 -0400

    Render responses with react-markdown

--- src/features/message/components/response-message.tsx
+++ src/features/message/components/response-message.tsx
@@ -1,4 +1,10 @@
+import ReactMarkdown from "react-markdown";
+
 // eslint-disable-next-line
 export default function ResponseMessage({ message }: { message: any }) {
-  return <div className="flex items-start">{message.value}</div>;
+  return (
+    <div className="flex flex-col items-start gap-2">
+      <ReactMarkdown>{message.value}</ReactMarkdown>
+    </div>
+  );
 }
