0a4bf001e33f774ef8de94b157467d24ec2a0a7a
Shawn Rodgers <me@bentsignal.com>
2025-06-11 20:52:53 -0400

    dynamic homepage for signed in / out users

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -1,15 +1,47 @@
 import { Button } from "@/components/ui/button";
 import { auth } from "@clerk/nextjs/server";
+import { Info } from "lucide-react";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default async function Home() {
   const { userId } = await auth();
+
   return (
     <>
-      <span>Welcome to QBE</span>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex items-center gap-2">
+          <span className="text-2xl font-bold">
+            {userId ? "Welcome back!" : "QBE"}
+          </span>
+          {!userId && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Info className="h-5 w-5" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className="font-semibold">
+                  Like &quot;cube&quot;, lol
+                </span>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </div>
+        <span className="text-muted-foreground text-lg font-semibold">
+          {userId
+            ? "How can I help you today?"
+            : "The first AI Chat app to embrace the z-axis."}
+        </span>
+      </div>
       {!userId && (
         <Button asChild>
-          <Link href="/login">Login</Link>
+          <Link href="/login" className="text-lg font-semibold">
+            Login
+          </Link>
         </Button>
       )}
     </>
