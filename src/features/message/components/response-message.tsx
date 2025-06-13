98c0eba14b3ee7a43497c8a9f6b8df7a30f57606
Shawn Rodgers <me@bentsignal.com>
2025-06-12 20:31:41 -0400

    increase cps

--- src/features/message/components/response-message.tsx
+++ src/features/message/components/response-message.tsx
@@ -5,7 +5,7 @@ import { markdownComponents } from "./markdown-components";
 import { useSmoothText } from "@convex-dev/agent/react";
 
 export default function ResponseMessage({ message }: { message: string }) {
-  const [text] = useSmoothText(message, { charsPerSec: 1000 });
+  const [text] = useSmoothText(message, { charsPerSec: 100000 });
   return (
     <div className="flex flex-col items-start gap-2">
       <ReactMarkdown
