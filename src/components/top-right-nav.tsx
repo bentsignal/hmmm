747c0402b655b27a13770bee5dbc6efea3345e9c
Shawn Rodgers <me@bentsignal.com>
2025-06-14 10:21:33 -0400

    add shadow to overlay components

--- src/components/top-right-nav.tsx
+++ src/components/top-right-nav.tsx
@@ -9,7 +9,9 @@ export default function TopRightNav() {
   const router = useRouter();
   const { theme, setTheme } = useTheme();
   return (
-    <div className={`bg-card m-4 flex w-fit items-center rounded-lg p-1`}>
+    <div
+      className={`bg-card m-4 flex w-fit items-center rounded-lg p-1 shadow-lg`}
+    >
       <Button
         variant="ghost"
         onClick={() => router.push("/settings")}
