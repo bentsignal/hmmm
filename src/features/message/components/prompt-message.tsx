ab7f689bded546d512be02133df07bcb212901ff
Shawn Rodgers <me@bentsignal.com>
2025-06-13 20:57:56 -0400

    adjusted fade in for prompts

--- src/features/message/components/prompt-message.tsx
+++ src/features/message/components/prompt-message.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function PromptMessage({ message }: { message: string }) {
+  const [opacity, setOpacity] = useState(0);
+
+  useEffect(() => {
+    setOpacity(1);
+  }, []);
+
   return (
-    <div className="fade-in">
+    <div
+      className="transition-opacity duration-1000 ease-in-out"
+      style={{ opacity }}
+    >
       <div
         className="bg-secondary text-secondary-foreground 
         flex max-w-xs flex-col gap-2 rounded-xl px-5 py-4"
