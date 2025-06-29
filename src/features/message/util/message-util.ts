2d83901db5267b5f1af3f18fff29bd5b9d9bbdee
Shawn Rodgers <me@bentsignal.com>
2025-06-29 14:42:15 -0400

    fixed issue with copying code

--- src/features/message/util/message-util.ts
+++ src/features/message/util/message-util.ts
@@ -1,15 +1,22 @@
 import { ReactNode } from "react";
 
-export function extractTextFromChildren(children: ReactNode) {
+export function extractTextFromChildren(children: ReactNode): string {
+  if (typeof children === "string") {
+    return children;
+  }
   if (Array.isArray(children)) {
-    const elements = children.map((child) => {
-      if (typeof child === "string") {
-        return child;
-      } else if (child.props.children) {
-        return String(child.props.children);
-      }
-    });
-    return elements.join("");
+    return children.map(extractTextFromChildren).join("");
+  }
+  if (
+    typeof children === "object" &&
+    children !== null &&
+    "props" in children &&
+    children.props &&
+    typeof children.props === "object" &&
+    children.props !== null &&
+    "children" in children.props
+  ) {
+    return extractTextFromChildren(children.props.children as ReactNode);
   }
   return "";
 }
