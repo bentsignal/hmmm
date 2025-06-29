1584d085be13d59fee0d2c256324057cad9a560b
Shawn Rodgers <me@bentsignal.com>
2025-06-29 18:54:32 -0400

    kill unnecessary component

--- src/features/message/components/streaming-message.tsx
+++ src/features/message/components/streaming-message.tsx
@@ -1,7 +1,7 @@
 import { MemoizedResponse } from "./response-message";
 import useStreamingMessage from "../hooks/use-streaming-message";
-import MessageWaiting from "./message-waiting";
 import useThreadStatus from "@/features/thread/hooks/use-thread-status";
+import { Loader2 } from "lucide-react";
 
 interface StreamingMessageProps {
   threadId: string;
@@ -18,7 +18,11 @@ export default function StreamingMessage({ threadId }: StreamingMessageProps) {
       <MemoizedResponse message={streamingMessage} streaming={!isThreadIdle} />
     );
   } else if (messageLength > 0 && !isThreadIdle) {
-    return <MessageWaiting />;
+    return (
+      <div className="flex items-center justify-start">
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </div>
+    );
   }
   return null;
 }
