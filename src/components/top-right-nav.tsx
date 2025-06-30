692ae074c43ec79578cd3657b1183cea12a798e1
Shawn Rodgers <me@bentsignal.com>
2025-06-30 13:54:02 -0400

    swapped theme and settings buttons

--- src/components/top-right-nav.tsx
+++ src/components/top-right-nav.tsx
@@ -12,19 +12,19 @@ export default function TopRightNav() {
     <div className="bg-card m-4 flex w-fit items-center rounded-lg border p-1 shadow-md">
       <Button
         variant="ghost"
-        onClick={() => router.push("/settings")}
-        size="icon"
-      >
-        <Settings className="h-4 w-4" />
-      </Button>
-      <Button
-        variant="ghost"
         size="icon"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       >
         <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
         <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         <span className="sr-only">Toggle theme</span>
+      </Button>
+      <Button
+        variant="ghost"
+        onClick={() => router.push("/settings")}
+        size="icon"
+      >
+        <Settings className="h-4 w-4" />
       </Button>
     </div>
   );
