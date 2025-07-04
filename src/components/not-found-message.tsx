d2db5a18fbf90157eefdbc7cab4422045536e8c9
shawn <me@bentsignal.com>
2025-07-04 14:34:54 -0400

    created premium and ultra colors for light mode, created custom 404 page

---
+++ src/components/not-found-message.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+import { Button } from "./ui/button";
+
+export default function NotFoundMessage() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-4 px-4 text-center">
+      <span className="text-2xl font-bold">Hmmmmm</span>
+      <span>
+        We had some trouble finding that page. If you think this is an error,
+        please{" "}
+        <span className="text-primary font-bold">
+          <Link href="/contact">contact support.</Link>
+        </span>
+      </span>
+      <Link href="/">
+        <Button>Return to home</Button>
+      </Link>
+    </div>
+  );
+}
