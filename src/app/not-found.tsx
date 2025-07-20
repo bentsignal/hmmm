e01e7f3270ef7414ed5fddc4f40f48f1fa832286
shawn <me@bentsignal.com>
2025-07-20 11:44:32 -0400

    not found update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,1 +1,20 @@
-export { default } from "@/components/not-found-message";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-4 px-4 text-center">
+      <span className="text-2xl font-bold">Hmmmmm</span>
+      <span>
+        We had some trouble finding that page. If you think this is an error,
+        please{" "}
+        <span className="text-primary font-bold">
+          <Link href="mailto:support@bsx.sh">contact support.</Link>
+        </span>
+      </span>
+      <Link href="/">
+        <Button>Return to home</Button>
+      </Link>
+    </div>
+  );
+}
