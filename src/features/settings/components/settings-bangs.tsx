be600a6d069a34deba9025d6338fdb5ac48380df
shawn <me@bentsignal.com>
2025-07-22 14:15:52 -0400

    settings bang uses new markdown component

--- src/features/settings/components/settings-bangs.tsx
+++ src/features/settings/components/settings-bangs.tsx
@@ -1,27 +1,24 @@
 "use client";
 
-import ReactMarkdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
-import remarkGfm from "remark-gfm";
-import { markdownComponents } from "@/features/messages/components/markdown-components";
+import { Markdown } from "@/components/ui/markdown";
+import { CopyButton } from "@/features/messages/components/copy-button";
 
 export default function SettingsBangs() {
-  const bang = "```bash\nhttps://www.qbe.sh/new?q=%s\n```";
+  const bangURL = "https://www.qbe.sh/new?q=%s";
+  const bang = `\`\n${bangURL}\n\`\n`;
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <span className="text-md font-bold mb-2">Search</span>
       <span className="text-sm text-muted-foreground">
         Add the following URL to your browser&apos;s search engine settings to
         create new chats directly from your browser&apos;s search bar.
       </span>
-      <div className="prose dark:prose-invert relative w-full max-w-full">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[rehypeHighlight]}
-          components={markdownComponents}
-        >
-          {bang}
-        </ReactMarkdown>
+      <div
+        className="prose dark:prose-invert relative w-full max-w-full 
+        flex flex-row items-center justify-start gap-2"
+      >
+        <Markdown className="prose dark:prose-invert">{bang}</Markdown>
+        <CopyButton getContent={() => bangURL} />
       </div>
     </div>
   );
