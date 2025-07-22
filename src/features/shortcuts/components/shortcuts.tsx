7225ad51dec03a616d753ef29d5dfe32158fd587
shawn <me@bentsignal.com>
2025-07-22 16:40:25 -0400

    shortcuts layout update

--- src/features/shortcuts/components/shortcuts.tsx
+++ src/features/shortcuts/components/shortcuts.tsx
@@ -6,7 +6,7 @@ export default function Shortcuts() {
     <div className="flex flex-col gap-2">
       {Object.values(shortcuts).map((shortcut) => (
         <div key={shortcut.name} className="flex flex-row gap-2">
-          <span className="font-bold min-w-24">{shortcut.name}</span>
+          <span className="font-bold min-w-36">{shortcut.name}</span>
           <span className="font-mono">
             {getShortcutString(shortcut.hotkey)}
           </span>
