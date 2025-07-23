c1538319ae878072b56d8db12957c156c7a644ec
shawn <me@bentsignal.com>
2025-07-23 18:53:05 -0400

    markdown in prompt messages

--- src/features/messages/components/prompt-message.tsx
+++ src/features/messages/components/prompt-message.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from "react";
 import { UIMessage } from "@convex-dev/agent/react";
 import { CopyButton } from "./copy-button";
+import { Markdown } from "@/components/ui/markdown";
 
 export default function PromptMessage({ message }: { message: UIMessage }) {
   const [hovering, setHovering] = useState(false);
@@ -13,10 +14,12 @@ export default function PromptMessage({ message }: { message: UIMessage }) {
     >
       <div
         className="bg-secondary text-secondary-foreground 
-        flex max-w-md flex-col gap-2 overflow-hidden rounded-xl px-5
+        flex max-w-lg flex-col gap-2 overflow-hidden rounded-xl px-5
         py-4 shadow-md"
       >
-        <div>{message.content}</div>
+        <Markdown className="prose dark:prose-invert relative w-full max-w-full">
+          {message.content}
+        </Markdown>
       </div>
       <div
         className="absolute right-0 -bottom-10 mt-2 flex justify-end transition-opacity duration-300 ease-in-out"
