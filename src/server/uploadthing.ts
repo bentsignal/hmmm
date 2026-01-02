8554852395fe6fe869d1127efa45b84b33619a6f
Shawn Rodgers <me@bentsignal.com>
2026-01-02 12:53:53 -0500

    upgrade convex-env to 2.2.0

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,9 +1,6 @@
+import { env } from "@/env";
 import { UTApi } from "uploadthing/server";
 
-if (!process.env.UPLOADTHING_TOKEN) {
-  throw new Error("UPLOADTHING_TOKEN not set");
-}
-
 export const utapi = new UTApi({
-  token: process.env.UPLOADTHING_TOKEN,
+  token: env.UPLOADTHING_TOKEN,
 });
