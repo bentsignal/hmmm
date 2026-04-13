c26923e35a88dd97cca1d0be8c3f94a0ccdf5f87
shawn <me@bentsignal.com>
2026-04-13 15:26:04 -0400

    fix env

--- apps/xr/src/env.ts
+++ apps/xr/src/env.ts
@@ -13,24 +13,19 @@ const runtimeEnv =
 export const env = createEnv({
   clientPrefix: "VITE_",
   server: {
-    // clerk
     CLERK_SECRET_KEY: z.string().min(1),
-    // convex
     CONVEX_DEPLOYMENT: z.string().min(1),
     CONVEX_INTERNAL_KEY: z.string().min(1),
-    // openrouter
     OPENROUTER_API_KEY: z.string().min(1),
-    // openai
     OPENAI_API_KEY: z.string().min(1),
+    CONVEX_DEPLOY_KEY: z.string().min(1).optional(),
   },
   client: {
     VITE_BASE_URL: z.string().min(1),
-    // clerk
     VITE_CLERK_PUBLISHABLE_KEY: z.string().min(1),
     VITE_CLERK_SIGN_IN_URL: z.string().min(1),
     VITE_CLERK_SIGN_UP_URL: z.string().min(1),
     VITE_CLERK_FRONTEND_API_URL: z.string().min(1),
-    // convex
     VITE_CONVEX_URL: z.string().min(1),
   },
   runtimeEnv,
